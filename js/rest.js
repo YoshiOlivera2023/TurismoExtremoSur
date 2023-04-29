735304f4b61c0e463f859d7108f823956527a041
Juan Carlos Toledo <jtoledo.externo@coopeuch.cl>
2023-04-28 22:18:30 -0400

    Implementacion de API 2

--- js/rest.js
+++ js/rest.js
@@ -1,9 +1,6 @@
 $(document).ready(function () 
 { 
 
-  $("#boton").click(function()
-  {
-
     var url = "http://api.weatherapi.com/v1/forecast.json?key=b50edbdcb6524dafa4a142215232804&q=Punta Arenas&days=10&aqi=no&alerts=no&lang=es";
 
     $.ajax({
@@ -27,18 +24,16 @@ $(document).ready(function ()
           let condition = day.day.condition.text;
 
 
-          dias_tiempo += "<div class='col'><div class='card h-100' style='width: 10rem;'><img src='http:"+imgen+"' class='card-img-top' alt='El tiempo'><div class='card-body'><h5 class='card-title'>"+date+" </h5><h3 class='card-title'>"+temp_c+" °</h3><p class='card-text'>"+condition+"</p></div></div></div>";     
+          dias_tiempo += "<div class='col' ><div class='card shadow-sm' style='width: 10rem;'><img src='http:"+imgen+"' class='card-img-top' alt='El tiempo'><div class='card-body'><h5 class='card-title'>"+date+" </h5><h3 class='card-title'>"+temp_c+" °</h3><p class='card-text'>"+condition+"</p></div></div></div>";     
         });
 
-        let eltiempo= "<div class='card' style='width: 18rem;'><img src='img/punta_arenas.jpg' class='card-img-top' alt='El tiempo'><div class='card-body'><h5 class='card-title'>"+city+"</h5><p class='card-text'>"+region+"</p></div></div>";
+        let eltiempo= "<div class='text-center'><div class='card' style='text-align: center;width: 18rem;' ><img src='img/punta_arenas.jpg' class='card-img-top' alt='El tiempo'><div class='card-body'><h5 class='card-title'>"+city+"</h5><p class='card-text'>"+region+"</p></div></div></div></div></div";
 
 
         $("#laciudad").html(eltiempo);
         $("#eltiempo").html(dias_tiempo);
       },
 
-      
-    });
   
 
   }); 
